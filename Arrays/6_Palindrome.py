6c2d8b3f82ccb9f8aa643301f4d1c4455edb33a0
Sakshi <iamsakshiingole@gmail.com>
2024-06-19 09:27:31 -0400

    5 code

--- Arrays/6_Palindrome.py
+++ Arrays/6_Palindrome.py
@@ -4,20 +4,19 @@ Given a Integer array A[] of n elements. Your task is to complete the function P
 Input:
 5
 111 222 333 444 555
+Output: 1'''
 
-Output:
-1'''
+class Solution:
+    def is_palindrome(self, num):
+        s = str(num)
+        return s == s[::-1]
 
-def is_palindrome(num):
-    s = str(num)
-    return s == s[::-1]
+    def PalinArray(self, arr, n):
+        for num in arr:
+            if not self.is_palindrome(num):
+                return 0
+        return 1
 
-def PalinArray(arr, n):
-    for num in arr:
-        if not is_palindrome(num):
-            return 0
-    return 1
-
-# Example usage
-print(PalinArray([111, 222, 333, 444, 555], 5))  # Output: 1
-print(PalinArray([121, 131, 20], 3))            # Output: 0
+sol = Solution()
+print(sol.PalinArray([111, 222, 333, 444, 555], 5))  # Output: 1
+print(sol.PalinArray([121, 131, 20], 3))  # Output: 0
