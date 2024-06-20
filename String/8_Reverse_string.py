2e22b751508439ca262db867b936aaf28c84ce0b
Sakshi <iamsakshiingole@gmail.com>
2024-06-20 09:36:49 -0400

    8 code

--- String/8_Reverse_string.py
+++ String/8_Reverse_string.py
@@ -4,3 +4,7 @@ Given a string s , return reverse of the string as output.
 Input: 
 s = "GeeksforGeeks"
 Output: "skeeGrofskeeG" '''
+
+class Solution:
+    def revStr (self, s : str) -> str :
+        return s[::-1]
